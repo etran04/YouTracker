af83907602582fca9ad618cea94b11e30ae35dd0
Eric Tran <erictritran@yahoo.com>
2018-06-14 14:34:35 -0700

    Initial implementation. Successful video playback

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '9.0'
+
+target 'YouTracker' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for YouTracker
+
+pod 'YouTubePlayer'
+pod 'SVProgressHUD'
+
+end
---
+++ YouTracker/Controllers/VideoTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  VideoTableViewCell.swift
+//  YouTracker
+//
+//  Created by Eric Tran on 6/14/18.
+//  Copyright © 2018 Eric Tran. All rights reserved.
+//
+
+import UIKit
+
+class VideoTableViewCell: UITableViewCell {
+
+    var url : String?
+    var title : String?
+    var startTime : String?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
